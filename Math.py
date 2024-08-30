ba3843720df13f2fb0d6a2267a7690e9ab167810
Zanila-drip <mleo0499@gmail.com>
2024-08-29 21:27:48 -0600

    Este es mi segundo commit que hago en github!

---
+++ Math.py
@@ -0,0 +1,17 @@
+"Suma"
+suma = 12 + 12
+"Resta"
+resta = 12 - 12
+"Multiplicacion"
+multiplicacion = 12 * 12
+"Division"
+division = 12 / 12
+"Mod"
+restante = 12 % 12
+
+print("Welcome to my math's!")
+print("Suma!",suma)
+print("Resta!",resta)
+print("Multiplicacion",multiplicacion)
+print("Division",division)
+print("Mod",restante)
